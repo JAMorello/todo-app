6d3b4fa22dc4e603443da23c14ac11f3724b6441
JAMorello <juan.ag.morello@gmail.com>
2021-08-05 16:04:42 -0300

    Added custom hooks folder

---
+++ src/customHooks/useToastAlert.js
@@ -0,0 +1,25 @@
+import { useToast } from "@chakra-ui/react";
+
+const useToastAlert = () => {
+  // Create a pop up alert
+  const alert = useToast();
+
+  // If the user did not entered a string for the taks, throw a pop up alert and do nothing else
+  const checkTask = (task) => {
+    let thrownAlert = false;
+    if (!task) {
+      alert({
+        title: "Task description missing",
+        status: "error",
+        duration: 3000,
+        isClosable: true,
+      });
+      return (thrownAlert = true);
+    }
+    return thrownAlert;
+  };
+
+  return checkTask;
+};
+
+export default useToastAlert;
